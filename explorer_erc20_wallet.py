55d576d71b24c9b4b9d26095cb5214ae9939815d
luizf <luizfilipe.limaxavier@gmail.com>
2022-09-06 17:52:17 -0300

    version_3

--- explorer_erc20_wallet.py
+++ explorer_erc20_wallet.py
@@ -1,5 +1,7 @@
 from config import database_infos
 from etherscan import Etherscan
+import pandas as pd
+from datetime import datetime
 
 eth = Etherscan(api_key=database_infos['api_key'])
 
@@ -15,4 +17,29 @@ atividade_transferencia_erc20_por_carteira = eth.get_erc20_token_transfer_events
     sort=sort
 )
 
-carteira_erc20 = atividade_transferencia_erc20_por_carteira
+# Setando em um DataFrame
+carteira_erc20_completa = pd.DataFrame(atividade_transferencia_erc20_por_carteira)
+
+# Setando coluna timestamp
+carteira_erc20_completa['timeStamp'] = pd.to_datetime(carteira_erc20_completa['timeStamp'], unit='s')
+
+# Selecionando somente colunas de interesse
+carteira_erc20 = carteira_erc20_completa[['tokenName', 'tokenSymbol',
+                                          'contractAddress', 'timeStamp', 'from', 'to', 'value']]
+
+# carteira_erc20['value'] = carteira_erc20['value'].astype(float)
+carteira_erc20 = carteira_erc20.assign(value=carteira_erc20['value'].str.replace(',', '.').astype(float))
+
+
+# PESQUISA DE HISTORICO DE MOEDAS/ENDEREÇOS ERC20 TRANSACIONADOS PELA CARTEIRA SELECIONADA SETADA PELO ENDEREÇO DE CONTRATO
+# realizar uma pesquisa de endereços de moedas e verificar as transações realizadas pela carteira referente.
+def filtro_contrato():
+
+    carteira_erc20_filtrado = carteira_erc20.copy()
+    carteira_erc20_filtrado = carteira_erc20_filtrado.set_index('contractAddress')
+
+    e_address = str(input())
+    filtro = carteira_erc20_filtrado.loc[e_address]
+    return print(filtro)
+
+filtro_contrato()
--- main.py
+++ main.py
@@ -1,6 +1,1 @@
 import api_etherscan
-
-
-
-
-print(api_etherscan.balanco_eth_carteira)
